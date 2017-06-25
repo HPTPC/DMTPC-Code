0f10754c32e2ea7cc34d8256330c17eeec1ac7c0
Wiliam Parker <wparker2@linappserv2.pp.rhul.ac.uk>
2017-06-25 15:46:10 +0100

    adding ccd_characterisation

---
+++ ccd_characterisation/calc_rms_C.d
@@ -0,0 +1,63 @@
+
+# DO NOT DELETE
+
+./calc_rms_C.so: /usr/include/root/TROOT.h /usr/include/root/TDirectory.h
+./calc_rms_C.so: /usr/include/root/TNamed.h /usr/include/root/TObject.h
+./calc_rms_C.so: /usr/include/root/Rtypes.h /usr/include/root/RConfig.h
+./calc_rms_C.so: /usr/include/root/RVersion.h /usr/include/root/DllImport.h
+./calc_rms_C.so: /usr/include/root/Rtypeinfo.h /usr/include/root/snprintf.h
+./calc_rms_C.so: /usr/include/root/strlcpy.h
+./calc_rms_C.so: /usr/include/root/TGenericClassInfo.h
+./calc_rms_C.so: /usr/include/root/TSchemaHelper.h
+./calc_rms_C.so: /usr/include/root/TStorage.h
+./calc_rms_C.so: /usr/include/root/TVersionCheck.h
+./calc_rms_C.so: /usr/include/root/Riosfwd.h /usr/include/root/TBuffer.h
+./calc_rms_C.so: /usr/include/root/TString.h /usr/include/root/TMathBase.h
+./calc_rms_C.so: /usr/include/root/TList.h /usr/include/root/TSeqCollection.h
+./calc_rms_C.so: /usr/include/root/TCollection.h
+./calc_rms_C.so: /usr/include/root/TIterator.h /usr/include/root/TDatime.h
+./calc_rms_C.so: /usr/include/root/TUUID.h /usr/include/root/TSystem.h
+./calc_rms_C.so: /usr/include/root/TInetAddress.h /usr/include/root/TTimer.h
+./calc_rms_C.so: /usr/include/root/TSysEvtHandler.h
+./calc_rms_C.so: /usr/include/root/TQObject.h /usr/include/root/TTime.h
+./calc_rms_C.so: /usr/include/root/ThreadLocalStorage.h
+./calc_rms_C.so: /usr/include/root/RConfigure.h /usr/include/root/TStyle.h
+./calc_rms_C.so: /usr/include/root/TAttAxis.h /usr/include/root/TAttLine.h
+./calc_rms_C.so: /usr/include/root/TAttFill.h /usr/include/root/TAttText.h
+./calc_rms_C.so: /usr/include/root/TAttMarker.h /usr/include/root/TArrayI.h
+./calc_rms_C.so: /usr/include/root/TArray.h /usr/include/root/TError.h
+./calc_rms_C.so: /usr/include/root/TFile.h /usr/include/root/TDirectoryFile.h
+./calc_rms_C.so: /usr/include/root/TMap.h /usr/include/root/THashTable.h
+./calc_rms_C.so: /usr/include/root/TUrl.h /usr/include/root/TTree.h
+./calc_rms_C.so: /usr/include/root/TBranch.h /usr/include/root/TObjArray.h
+./calc_rms_C.so: /usr/include/root/TDataType.h
+./calc_rms_C.so: /usr/include/root/TDictionary.h /usr/include/root/Property.h
+./calc_rms_C.so: /usr/include/root/ESTLType.h /usr/include/root/TArrayD.h
+./calc_rms_C.so: /usr/include/root/TClass.h /usr/include/root/TObjString.h
+./calc_rms_C.so: /usr/include/root/TVirtualTreePlayer.h
+./calc_rms_C.so: /usr/include/root/TH1.h /usr/include/root/TAxis.h
+./calc_rms_C.so: /usr/include/root/TArrayC.h /usr/include/root/TArrayS.h
+./calc_rms_C.so: /usr/include/root/TArrayF.h /usr/include/root/Foption.h
+./calc_rms_C.so: /usr/include/root/TVectorFfwd.h
+./calc_rms_C.so: /usr/include/root/TVectorDfwd.h
+./calc_rms_C.so: /usr/include/root/TFitResultPtr.h /usr/include/root/TH2.h
+./calc_rms_C.so: /usr/include/root/TMatrixFBasefwd.h
+./calc_rms_C.so: /usr/include/root/TMatrixDBasefwd.h
+./calc_rms_C.so: /usr/include/root/TCanvas.h /usr/include/root/TPad.h
+./calc_rms_C.so: /usr/include/root/TVirtualPad.h /usr/include/root/TAttPad.h
+./calc_rms_C.so: /usr/include/root/TVirtualX.h /usr/include/root/GuiTypes.h
+./calc_rms_C.so: /usr/include/root/Buttons.h /usr/include/root/TAttBBox2D.h
+./calc_rms_C.so: /usr/include/root/TPoint.h /usr/include/root/TAttCanvas.h
+./calc_rms_C.so: /usr/include/root/TCanvasImp.h /usr/include/root/TRandom3.h
+./calc_rms_C.so: /usr/include/root/TRandom.h /usr/include/root/TF1.h
+./calc_rms_C.so: /usr/include/root/TFormula.h /usr/include/root/TBits.h
+./calc_rms_C.so: /usr/include/root/Math/ParamFunctor.h
+./calc_rms_C.so: /usr/include/root/TColor.h
+./calc_rms_C.so: /scratch2/gmuraru/dmtpc_software/DmtpcCore/include/CameraInfo.hh
+./calc_rms_C.so: /scratch2/gmuraru/dmtpc_software/DmtpcCore/include/Event.hh
+./calc_rms_C.so: /scratch2/gmuraru/dmtpc_software/DmtpcCore/include/Image.hh
+./calc_rms_C.so: /scratch2/gmuraru/dmtpc_software/DmtpcCore/include/DatasetHeader.hh
+./calc_rms_C.so: /usr/include/root/TTimeStamp.h
+./calc_rms_C.so: /scratch2/gmuraru/dmtpc_software/DmtpcCore/include/Dataset.hh
+./calc_rms_C.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+calc_rms_C__ROOTBUILDVERSION= 5.34/36
