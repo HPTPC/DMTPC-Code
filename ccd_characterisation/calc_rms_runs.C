95fa22292b8af722533fcf5046fac15d95587540
onichandame <zxinmyth@gmail.com>
2017-08-01 13:32:35 +0100

    Added 1 argument
    Added the third argument pixval and moved the argument dir to the forth. if pixval=0, intensity value of individual pixels will be discarded to save disk space. By default it is 0.

--- ccd_characterisation/calc_rms_runs.C
+++ ccd_characterisation/calc_rms_runs.C
@@ -1,98 +1,151 @@
-void calc_rms_runs(int runs)
-{
+void calc_rms_runs(int run1 = 1167004, int runs = 1, Bool_t pixflag=0, const char * dir = "/scratch3/wparker2/dmtpc2/data/2017/06/raw")
+{// runs = number of runs you want to consider 
+//run1 is the runID of the first one 
+//pixflag turns branch pixflag on and off. If it's set to 1, all intensity values of all pixels are saved
+  //in the branch pixval. The size of output file is inflated by more than 1000 times
+//dir is the path to the directory with the raw data files in it
 
-  gSystem->Load("/scratch3/wparker2/dmtpc_software/DmtpcCore/lib/libDmtpcCore.so");
-  gSystem->Load("/scratch3/wparker2/dmtpc_software/DmtpcAnalysis/lib/libDmtpcAnalysis.so");
+  gSystem->Load("$DMTPC_HOME/DmtpcCore/lib/libDmtpcCore.so");
+  gSystem->Load("$DMTPC_HOME/DmtpcAnalysis/lib/libDmtpcAnalysis.so");
 
   double stops[5] = {0,0.34,0.61,0.84,1.0};
-  gStyle->SetOptStat(0); 
+  gStyle->SetOptStat(0);
   double red[5] = {0.0,0.0,0.87,1.0,0.51};
   double green[5] = {0.0,0.81,1.00,0.2,0.0};
   double blue[5] = {0.51,1.0,0.12,0.0,0.0};
   TColor::CreateGradientColorTable(5,stops,red,green,blue,255);
   gStyle->SetNumberContours(255);
 
-  const char * dir ="/scratch3/wparker2/dmtpc2/data/2017/06/raw/";  
-  int cam = 1;
+  //From here starts the analysis
+  int cam = 0;
   int ncam = 1;
 
   for (int fi=0;fi<runs;fi++){
-   
-    d = new dmtpc::core::Dataset;
-    int run=1038014;
-    d->open(TString::Format("%s/hptpc_test_R%05d.raw.root",dir,run+fi)); 
-    TH2 * bias = d->biasAvg(cam);
-    
-    int n = d->nevents(); 
-    TH2 * image = new TH2D("image","image", d->event()->ccdData(cam)->GetNbinsX(), 
-			   d->event()->ccdData(cam)->GetXaxis()->GetXmin(), 
-			   d->event()->ccdData(cam)->GetXaxis()->GetXmax(), 
-			   d->event()->ccdData(cam)->GetNbinsY(), 
-			   d->event()->ccdData(cam)->GetYaxis()->GetXmin(), 
-			   d->event()->ccdData(cam)->GetYaxis()->GetXmax());
-    TFile * calc_rms1 = new TFile("calc_rms1.root","RECREATE");
-    TTree t("pix_t","Pixel Tree");
-    int nbins = 1000;
-    // TF1 * gFit = new TF1("gFit","gaus");
-    
-    TH1F* intensity=new TH1F("intensity","intensity",nbins,-100,100);
-    double rms = 0;
-    double mean = 0;
-    double chi = 0;
-    int runnum = 0;
-    double ccdTemp;
-    int nevents = 0;
-    
-    //t.Branch("intensity",&intensity);
+    //loop over the number of runs you want to consider
+
+    TFile f(TString::Format("%s/hptpc_test_R%05d.raw.root",dir,run1+fi));
+    if (f.IsZombie()) { //check that the file opens
+    cout<<"file with run number "<< run1+fi<<" is a zombie"<<endl;
+    continue; //go onto next run if it doesn't
+    }
+    else {
+
+     //open the data from the file into a dataset
+     d = new dmtpc::core::Dataset;
+     d->open(TString::Format("%s/hptpc_test_R%05d.raw.root",dir,run1+fi));
+
+     //output file
+     TString output_name = Form("calc_rms_R%i.root",run1+fi);
+     TFile * calc_rms1 = new TFile(output_name,"RECREATE");
+     TTree t("pix_t","Pixel Tree");
+     int nbins = 200;
+
+     if (!d) {//check the dataset exists
+       std::cout<<"no d"<<std::endl;
+       continue;
+     }
+ 
+     TH2 * bias = d->biasAvg(cam);
+ 
+     int n = d->nevents(); //get the number of events in the run to loop over 
+     TH2 * image = new TH2D("image","image", d->event()->ccdData(cam)->GetNbinsX(),
+       d->event()->ccdData(cam)->GetXaxis()->GetXmin(),
+       d->event()->ccdData(cam)->GetXaxis()->GetXmax(),
+       d->event()->ccdData(cam)->GetNbinsY(),
+       d->event()->ccdData(cam)->GetYaxis()->GetXmin(),
+       d->event()->ccdData(cam)->GetYaxis()->GetXmax());
+
+     TH1D* intensity=new TH1D("intensity","intensity",nbins,-100,100);
+     //intensity histogram to hold all the data from every pixel of every event
+     double rms = 0;
+     double mean = 0;
+     double chi = 0;
+     int runnum = 0;
+     double ccdTemp;
+     int nevents = 0;
+ 
+    //This block creates a variable to store pixval data.
+    const int stupidcppx = (int)(d->event()->ccdData(cam)->GetNbinsX());
+    const int stupidcppy = (int)(d->event()->ccdData(cam)->GetNbinsY());
+    const int x = stupidcppx;
+    const int y = stupidcppy;
+    Float_t pixval[x][y];
+
+    //create branches for the variables you want to save to the tree
     t.Branch("mean",&mean);
     t.Branch("rms",&rms);
-    // t.Branch("runnum",&runnum);
     t.Branch("nevents",&nevents);
     t.Branch("chi",&chi);
+    t.Branch("runnum",&runnum);
     t.Branch("ccdTemp",&ccdTemp);
-    
-    for (int i = 0; i < d->nevents(); i++)
-      {
-	d->getEvent(i);
-	//runnum = d->event()->runNumber();
-	nevents = d->nevents();
-   
-	image = d->event()->getImage(cam)->getVisible(); 
-	image->Add(d->biasAvg(cam),-1);
-	ccdTemp = d->event()->ccdConfig(cam)->ccdTemp;
-	
-	for (int m=0; m<image->GetNbinsX(); m++){
-	  for (int n=0; n<image->GetNbinsY(); n++) {
-	    float pixval=image->GetBinContent(m,n);
-	    
-	    intensity->Fill(pixval);
-	  }
-	}
-	TF1 * gFit = new TF1("gFit","gaus"); 
-	intensity->Fit(gFit);
-	rms = intensity->GetRMS(); //gFit->GetParameter(2);
-	mean = gFit->GetParameter(1);
-	chi = gFit->GetChisquare();
-	
-	t.Fill();
-      }  
-  
+  //create branch pixval only if pixflag is set to 1
+    if (pixflag==1)
+    {
+      const char *leaflist = (char*)TString::Format("pixval[%i][%i]/F",x,y);
+      t.Branch("pixval",&pixval[0][0],leaflist);
+    }
+
+    std::cout<< n <<" events."<<std::endl;
+
+    for (int i = 0; i < n; i++) { //loop over each event
+      std::cout<<endl;
+      std::cout<<"Event "<<i+1<<std::endl;
+      TH1D *eventintensity = new TH1D("eventintensity","eventintensity",nbins,-100,100);
+      //this histogram only holds the pixel values from each event and is made new each iteration
+      eventintensity->Sumw2();
+      d->getEvent(i);
+      nevents = d->nevents();
+      runnum = run1+fi;
+      image = d->event()->getImage(cam)->getVisible();
+      image->Add(d->biasAvg(cam),-1);
+
+      ccdTemp = d->event()->ccdConfig(cam)->ccdTemp;
+
+      for (int m=0; m<image->GetNbinsX(); m++){
+        for (int b=0; b<image->GetNbinsY(); b++) {
+          Float_t temppixval=image->GetBinContent(m,b);
+
+          //fill temppixval to pixval[][] if pixflag is true(1)
+          if (pixflag==1)
+          {
+            pixval[m][b]=temppixval;
+          }
+
+         //fill both intenstiy hisotgrams with the pixel data
+         intensity->Fill(temppixval);
+         eventintensity->Fill(temppixval);
+       }
+     }
+     //gaussian is fitted to the data from event i 
+     TF1 * gFit = new TF1("gFit","gaus");
+     eventintensity->Fit(gFit,"WL");
+     rms = eventintensity->GetRMS();
+     mean = gFit->GetParameter(1);
+     chi = gFit->GetChisquare();
+
+      t.Fill(); //data added to the tree
+      delete eventintensity;
+      delete gFit;
+    }
+    //fit gaussian to all data
+    TF1 * gFit2 = new TF1("gFit2","gaus");
+    intensity->Fit(gFit2);
+
+    double rmsfinal = intensity->GetRMS();
+    std::cout<<"The rms of the intensity is "<<rmsfinal<<"."<<std::endl;
+
     intensity->SetStats(kTRUE);
     gStyle->SetOptStat(1111);
-    intensity->SetXTitle("ADU"); 
+    intensity->SetXTitle("ADU");
     intensity->SetYTitle("Number of Entries");
-    
-    t->Write();
-    // TCanvas *c2 = new TCanvas("c2","",0,0,600,600);
-    // c2->cd();
-    // intensity->Draw();
-    // intensity->Write();
-    // c2->Update();
-    // c2->SetLogy();
-    
-    // Fe55_gain->Write();
-    // Fe55_gain->Close();
-  }  
+
+    //write tree to output file wherever you run the code from
+    t.Write();
+    image->Write();
+    intensity->Write();
+    bias->Write();
+
+    }
+
+  }
 }
-
-
